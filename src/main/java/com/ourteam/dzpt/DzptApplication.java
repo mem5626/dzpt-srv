473a0ec9e6e25cfcddba62452ae7d737ce08fd44
Allan <mem5626@qq.com>
2019-07-10 05:05:07 +0800

    initial commit

---
+++ src/main/java/com/ourteam/dzpt/DzptApplication.java
@@ -0,0 +1,13 @@
+package com.ourteam.dzpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzptApplication.class, args);
+	}
+
+}
